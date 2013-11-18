ad028587b5f78dd392475d60f3140382789a9876
Theo Vora <theevo@gmail.com>
2013-11-18 18:53:33 -0500

    scrape name

--- lib/player_scraper.rb
+++ lib/player_scraper.rb
@@ -1,7 +1,20 @@
 class PlayerScraper
-  attr_accessor :player_data
+  attr_accessor :player_data, :name
 
   def initialize(url)
     @player_data = Nokogiri::HTML(open(url))
   end
+
+  def get_name
+    name = @player_data.css("div.mod-content h1").text
+  end
+
+# OTHER ATTRIBUTES
+# picture
+# stats from last game
+#   - make the game linkable!
+# next game date, against who, time, where?
+# current season stats (all)
+
+
 end
--- scrape_jlin.rb
+++ scrape_jlin.rb
@@ -2,5 +2,7 @@ require_relative './config/environment.rb'
 
 jlin_url = "http://espn.go.com/nba/player/_/id/4299/jeremy-lin"
 
-scaper = PlayerScraper.new(jlin_url)
+scraper = PlayerScraper.new(jlin_url)
+jlin_name = scraper.get_name
 
+binding.pry
