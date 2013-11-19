e36bd361cd9d03e573cc76eb2bcaa757ecf2a9c0
jamesjtong <jtong33@gmail.com>
2013-11-18 21:03:01 -0500

    added working last few games stats scraper method

--- lib/player_scraper.rb
+++ lib/player_scraper.rb
@@ -6,11 +6,30 @@ class PlayerScraper
   end
 
   def get_name
-    name = @player_data.css("div.mod-content h1").text
+    @name = @player_data.css("div.mod-content h1").text
   end
 
+  def get_picture
+    @picture = @player_data.css("div.main-headshot img").attr("src").value
+  end
+
+  def get_last_game_stats
+    @last_game_stats = @player_data.css('tr[class*="team-46"]').map{ |tr|
+  tr.css('td').map(&:text)}
+  end
+
+
+  def get_last
+    @last_game_stats = @player_data.css('')
+  end
+
+
+  # 'div[id^="para-"]')
+  # class="oddrow team-46-19"
+  #   'tr[class^="oddrow team-46"]'
+
 # OTHER ATTRIBUTES
-# picture
+
 # stats from last game
 #   - make the game linkable!
 # next game date, against who, time, where?
