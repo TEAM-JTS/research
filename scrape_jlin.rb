2bd93194354bc6ab738889522a71bed2a4d89e3b
Theo Vora <theevo@gmail.com>
2013-11-19 15:49:34 -0500

    mechanize works

--- scrape_jlin.rb
+++ scrape_jlin.rb
@@ -1,8 +1,15 @@
 require_relative './config/environment.rb'
 
-jlin_url = "http://espn.go.com/nba/player/_/id/4299/jeremy-lin"
+agent = Mechanize.new
+page = agent.get('http://search.espn.go.com/jeremy-lin')
 
-scraper = PlayerScraper.new(jlin_url)
-jlin_name = scraper.get_name
+# page.links_with(:text => 'Jeremy Lin').each do |link|
+#   puts link.href
+# end
 
-binding.pry
+puts page.links_with(:text => 'Jeremy Lin').first.href
+
+# jlin_url = "http://espn.go.com/nba/player/_/id/4299/jeremy-lin"
+
+# scraper = PlayerScraper.new(jlin_url)
+# jlin_name = scraper.get_name
--- yql_test.rb
+++ yql_test.rb
@@ -1,11 +1,14 @@
 require 'pry'
 require 'net/http'
 require 'oj'
-
+require 'yahoo_nba'
+require 'awesome_print'
 
 url = 'http://query.yahooapis.com/v1/public/yql?q=desc%20fantasysports.players.stats&format=json&diagnostics=true&callback='
-resp = Net::HTTP.get_response(URI.parse(url)) # get_response takes an URI object
+query = YahooNba::Query.new ENV['YAHOO_OAUTH_KEY'], ENV['YAHOO_OAUTH_SECRET']
 
-data = resp.body
-hash = Oj.load(data)
-binding.pry
+player_keys_hash = query.get_all_player_keys_hash
+
+player_keys_hash.each do |player, id|
+  puts "First name: #{player.split(" ").first} / Last name: #{player.split(" ").last}"
+end
