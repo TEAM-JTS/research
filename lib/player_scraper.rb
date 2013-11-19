c2ace17512582542fbd655ec284226f148e03ca5
jamesjtong <jtong33@gmail.com>
2013-11-19 11:46:28 -0500

    p

--- lib/player_scraper.rb
+++ lib/player_scraper.rb
@@ -1,5 +1,5 @@
 class PlayerScraper
-  attr_reader :player_data, :name, :picture, :most_recent_0, :most_recent_1,:most_recent_2,:most_recent_3,:most_recent_4, :season_averages_hash, :game_day, :game_time
+  attr_reader :player_data, :name, :picture, :most_recent_0, :most_recent_1,:most_recent_2,:most_recent_3,:most_recent_4, :season_averages_hash, :career_averages_hash, :game_day, :game_time
 
   def initialize(url)
     @player_data = Nokogiri::HTML(open(url))
@@ -15,16 +15,21 @@ class PlayerScraper
 
   def get_last_game_stats
     @last_game_stats = @player_data.css('tr[class*="team-46"]').map{ |tr|tr.css('td').map(&:text)}
-    @most_recent_0 = {date: @last_game_stats[0][0], vs: @last_game_stats[0][1], score: @last_game_stats[0][2], minutes: @last_game_stats[0][3], fgam: @last_game_stats[0][4], threepointam: @last_game_stats[0][6], ftam: @last_game_stats[0][8], rb: @last_game_stats[0][10], ast: @last_game_stats[0][11], blk: @last_game_stats[0][12], stl: @last_game_stats[0][13], to: @last_game_stats[0][15], pts: @last_game_stats[0][16]}
-    @most_recent_1 = {date: @last_game_stats[1][0], vs: @last_game_stats[1][1], score: @last_game_stats[1][2], minutes: @last_game_stats[1][3], fgam: @last_game_stats[1][4], threepointam: @last_game_stats[1][6], ftam: @last_game_stats[1][8], rb: @last_game_stats[1][10], ast: @last_game_stats[1][11], blk: @last_game_stats[1][12], stl: @last_game_stats[1][13], to: @last_game_stats[1][15], pts: @last_game_stats[1][16]}
-    @most_recent_2 = {date: @last_game_stats[2][0], vs: @last_game_stats[2][1], score: @last_game_stats[2][2], minutes: @last_game_stats[2][3], fgam: @last_game_stats[2][4], threepointam: @last_game_stats[2][6], ftam: @last_game_stats[2][8], rb: @last_game_stats[2][10], ast: @last_game_stats[2][11], blk: @last_game_stats[2][12], stl: @last_game_stats[2][13], to: @last_game_stats[2][15], pts: @last_game_stats[2][16]}
-    @most_recent_3 = {date: @last_game_stats[3][0], vs: @last_game_stats[3][1], score: @last_game_stats[3][2], minutes: @last_game_stats[3][3], fgam: @last_game_stats[3][4], threepointam: @last_game_stats[3][6], ftam: @last_game_stats[3][8], rb: @last_game_stats[3][10], ast: @last_game_stats[3][11], blk: @last_game_stats[3][12], stl: @last_game_stats[3][13], to: @last_game_stats[3][15], pts: @last_game_stats[3][16]}
-    @most_recent_4 = {date: @last_game_stats[4][0], vs: @last_game_stats[4][1], score: @last_game_stats[4][2], minutes: @last_game_stats[4][3], fgam: @last_game_stats[4][4], threepointam: @last_game_stats[4][6], ftam: @last_game_stats[4][8], rb: @last_game_stats[4][10], ast: @last_game_stats[4][11], blk: @last_game_stats[4][12], stl: @last_game_stats[4][13], to: @last_game_stats[4][15], pts: @last_game_stats[4][16]}
+    @most_recent_0 = {date: @last_game_stats[0][0], vs: @last_game_stats[0][1], score: @last_game_stats[0][2], minutes: @last_game_stats[0][3], fgam: @last_game_stats[0][4], threepointam: @last_game_stats[0][6], ftam: @last_game_stats[0][8], reb: @last_game_stats[0][10], ast: @last_game_stats[0][11], blk: @last_game_stats[0][12], stl: @last_game_stats[0][13], to: @last_game_stats[0][15], pts: @last_game_stats[0][16]}
+    @most_recent_1 = {date: @last_game_stats[1][0], vs: @last_game_stats[1][1], score: @last_game_stats[1][2], minutes: @last_game_stats[1][3], fgam: @last_game_stats[1][4], threepointam: @last_game_stats[1][6], ftam: @last_game_stats[1][8], reb: @last_game_stats[1][10], ast: @last_game_stats[1][11], blk: @last_game_stats[1][12], stl: @last_game_stats[1][13], to: @last_game_stats[1][15], pts: @last_game_stats[1][16]}
+    @most_recent_2 = {date: @last_game_stats[2][0], vs: @last_game_stats[2][1], score: @last_game_stats[2][2], minutes: @last_game_stats[2][3], fgam: @last_game_stats[2][4], threepointam: @last_game_stats[2][6], ftam: @last_game_stats[2][8], reb: @last_game_stats[2][10], ast: @last_game_stats[2][11], blk: @last_game_stats[2][12], stl: @last_game_stats[2][13], to: @last_game_stats[2][15], pts: @last_game_stats[2][16]}
+    @most_recent_3 = {date: @last_game_stats[3][0], vs: @last_game_stats[3][1], score: @last_game_stats[3][2], minutes: @last_game_stats[3][3], fgam: @last_game_stats[3][4], threepointam: @last_game_stats[3][6], ftam: @last_game_stats[3][8], reb: @last_game_stats[3][10], ast: @last_game_stats[3][11], blk: @last_game_stats[3][12], stl: @last_game_stats[3][13], to: @last_game_stats[3][15], pts: @last_game_stats[3][16]}
+    @most_recent_4 = {date: @last_game_stats[4][0], vs: @last_game_stats[4][1], score: @last_game_stats[4][2], minutes: @last_game_stats[4][3], fgam: @last_game_stats[4][4], threepointam: @last_game_stats[4][6], ftam: @last_game_stats[4][8], reb: @last_game_stats[4][10], ast: @last_game_stats[4][11], blk: @last_game_stats[4][12], stl: @last_game_stats[4][13], to: @last_game_stats[4][15], pts: @last_game_stats[4][16]}
   end
 
   def get_season_averages
     @season_averages_array = @player_data.css('.oddrow').map{|row| row.css('td').map(&:text)}[2]
     @season_averages_hash = {gp: @season_averages_array[1], mpg: @season_averages_array[2], fgp: @season_averages_array[4], threepointp: @season_averages_array[6], ftp: @season_averages_array[8], rb: @season_averages_array[9], ast: @season_averages_array[10], blk: @season_averages_array[11], stl: @season_averages_array[12], to: @season_averages_array[14], pts: @season_averages_array[15]}
+  end
+
+  def get_career_averages
+    @career_averages_array = @player_data.css('.evenrow').map{|row| row.css('td').map(&:text)}[2]
+    @career_averages_hash = {gp: @career_averages_array[1], mpg: @career_averages_array[2], fgp: @career_averages_array[4], threepointp: @career_averages_array[6], ftp: @career_averages_array[8], rb: @career_averages_array[9], ast: @career_averages_array[10], blk: @career_averages_array[11], stl: @career_averages_array[12], to: @career_averages_array[14], pts: @career_averages_array[15]}
   end
 
   def get_next_game_info
